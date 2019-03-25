bde41dd6ecac347a72cbc57ba4187c8da6c2c51e
Nicolás Morales <nicomorales297@gmail.com>
2019-03-25 12:13:40 -0300

    useLayoutEffect

--- src/context/AuthState.js
+++ src/context/AuthState.js
@@ -1,35 +1,43 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect,useState } from "react";
 import authReducer from "./reducers/authentication.reducer";
 import { setCurrentUser } from "./actions/authentication.action";
 import AuthStateGlobal from "./AuthStateGlobal";
 import jwt_decode from "jwt-decode";
 
-
-const AuthState = (props) => {
-
-    const [stateUser, dispatch] = useReducer(authReducer, {//useReducer recibe un reducer y un estado inicial. Y devuelve el estado actual y el método dispatch. El stateUser viene del return del reducer
+const AuthState = props => {
+    const [stateUser, dispatch] = useReducer(authReducer, {
+        //useReducer recibe un reducer y un estado inicial. Y devuelve el estado actual y el método dispatch. El stateUser viene del return del reducer
         isAuthenticated: null,
         user: {}
     });
-    
-    useEffect(()=>{//esto es como un componentdidmount pero con hooks. El hook use effect se ejecuta en cada render
-        if (localStorage.jwt) {//reviso si hay un token en el localStorage
-            const decoded = localStorage.jwt?localStorage.jwt:"";
+    const [showChild, setShowChild] = useState(false);
+
+    useEffect(() => {
+        //esto es como un componentdidmount pero con hooks. El hook use effect se ejecuta en cada render
+        if (localStorage.jwt) {
+            //reviso si hay un token en el localStorage
+            const decoded = localStorage.jwt ? localStorage.jwt : "";
             dispatch(setCurrentUser(jwt_decode(decoded))); //si hay un token entonces lo decodifico y hago un dispatch del action setCurrentUser
+            
         }
-
-    },[])// estas llaves cuadradas significa que este hook sólo se ejecutará al montarse y al desmontarse, y no dependen del state. Entonces esto se ejecutará sólo la primera vez que se ejecuta nuestra Aplicación (cuando se monta el componente). Si no estuvieran estas llaves entonces se ejecutaría siempre que hay un cambio
-
-    return (//acá envuelvo mi mi aplicación para entregar el state a todos los componentes que quieran consumirla. Se deve poner la palabra Provider, para pasar el context a componentes hijos
-        <AuthStateGlobal.Provider
-            value={{// acá envío lo que quiero que mis demás componentes quieren consumir, en este caso devuelvo la info del usuario más un booleano para ver si está logueado, y también paso el método dispatch para que mis demás componentes puedan ejecutar actions que llaman al reducer. Como es el caso del logout
-                stateUser,
-                dispatch
-            }}
-        >
-           {props.children}
-        </AuthStateGlobal.Provider>
-    );
+        setShowChild(true);
+    }, []); // estas llaves cuadradas significa que este hook sólo se ejecutará al montarse y al desmontarse, y no dependen del state. Entonces esto se ejecutará sólo la primera vez que se ejecuta nuestra Aplicación (cuando se monta el componente). Si no estuvieran estas llaves entonces se ejecutaría siempre que hay un cambio
+    if (!showChild) {
+        return null;
+    } else {
+        return (
+            //acá envuelvo mi mi aplicación para entregar el state a todos los componentes que quieran consumirla. Se deve poner la palabra Provider, para pasar el context a componentes hijos
+            <AuthStateGlobal.Provider
+                value={{
+                    // acá envío lo que quiero que mis demás componentes quieren consumir, en este caso devuelvo la info del usuario más un booleano para ver si está logueado, y también paso el método dispatch para que mis demás componentes puedan ejecutar actions que llaman al reducer. Como es el caso del logout
+                    stateUser,
+                    dispatch
+                }}
+            >
+                {props.children}
+            </AuthStateGlobal.Provider>
+        );
+    }
 };
 
 export default AuthState;
--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useLayoutEffect, useContext, useState } from "react";
 import AuthStateGlobal from "../../context/AuthStateGlobal";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -8,27 +8,28 @@ import Col from "react-bootstrap/Col";
 import { loginUser } from "../../context/actions/authentication.action";
 
 const Login = props => {
-    const context = useContext(AuthStateGlobal);//recibo el context
+    const context = useContext(AuthStateGlobal); //recibo el context
     const [rut_usuario, setrutusuario] = useState(""); // este hook se usa para el estado del componente, es lo mismo que hacíamos cuando declarabamos un constructor y un state, esto recibe un estado inicial y devuelve el estado actual y una función para setear el estado, lo mismo que un setState
     const [clave_usuario, setclaveusuario] = useState("");
 
-    useEffect(() => {// si está logueado no lo dejo ver este componente y lo saco al dashboard
-        if (context.stateUser.isAuthenticated===true) {
+    useLayoutEffect(() => {
+        // si está logueado no lo dejo ver este componente y lo saco al dashboard
+        if (context.stateUser.isAuthenticated === true) {
             props.history.push("/");
         }
     });
 
-    const handleSubmit = e => {//acá controlo el formulario
+    const handleSubmit = e => {
+        //acá controlo el formulario
         const user = {
             rut_usuario,
             clave_usuario
         };
-        loginUser(user, context.dispatch);// ejecuto el action loginUser pasándole el usuario, y el método dispatch que viene del provider y el hook useReducer
-        e.preventDefault();// no recargo
+        loginUser(user, context.dispatch); // ejecuto el action loginUser pasándole el usuario, y el método dispatch que viene del provider y el hook useReducer
+        e.preventDefault(); // no recargo
     };
- 
+
     return (
-        
         <Container>
             <Row>
                 <Col md={4}>
